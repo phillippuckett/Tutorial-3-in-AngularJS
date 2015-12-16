70f3fdddb2d2abc1b12abb96411e191e051d803d
Phillip Puckett <phillippuckett88@gmail.com>
2015-12-16 15:34:14 -0700

    Initial Push

---
+++ chat-room/js/parseService.js
@@ -0,0 +1,39 @@
+angular.module('chatroom').service('parseService', function ($http) {
+
+  // REST = A protocol for web requests, which stands for Representational State Transfer //
+
+  //On the lines below create a getData method. This method will retrieve data from the parse backend.
+  //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
+  //Be sure to return whatever gets returned from $http so you can call .then in your controller.
+
+  //On the line below create the postData method. This method will add data to the parse backend.
+  //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
+  //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
+  //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
+  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+  
+  // getData method //
+  this.getData = function () {
+    return $http({
+      method: 'GET',
+      url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+    }).then(function (result) {
+      console.log("this is the result from the server(in my service)", result);
+      console.log("after we've drilled down", result.data.results);
+      return result.data.results;
+    });
+  };
+
+  // postData method //
+  this.postData = function (yourMessage) {
+    //        alert(yourMessage); 
+    return $http({
+      method: 'POST',
+      url: 'https://api.parse.com/1/classes/chat',
+      data: {
+        text: yourMessage
+      }
+    });
+  };
+
+});
---
+++ user-profiles-ii/controller.js
@@ -0,0 +1,8 @@
+angular.module('userProfiles').controller('MainController', function ($scope, mainService) {
+	$scope.getUsers = function () {
+		mainService.getUsers().then(function (response) {
+			$scope.users = response.data.data;
+		});
+	}
+	$scope.getUsers();
+});
